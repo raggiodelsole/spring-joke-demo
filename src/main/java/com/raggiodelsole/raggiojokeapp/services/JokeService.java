a2cba5f1825bfe94148f1f428d3103ba83fa0350
raggiodelsole <radoslaw.kurowski.94@gmail.com>
2020-06-28 15:41:53 +0200

    after video changes

--- src/main/java/com/raggiodelsole/raggiojokeapp/services/JokeService.java
+++ src/main/java/com/raggiodelsole/raggiojokeapp/services/JokeService.java
@@ -1,11 +1,5 @@
 package com.raggiodelsole.raggiojokeapp.services;
 
-import guru.springframework.norris.chuck.ChuckNorrisQuotes;
-import org.springframework.stereotype.Service;
-
-@Service
-public class JokeService {
-    public String getJoke() {
-        return new ChuckNorrisQuotes().getRandomQuote();
-    }
+public interface JokeService {
+    String getJoke();
 }
